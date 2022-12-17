0d6fa145f3ba65d5ea119b477ec84c993704604e
Nnadi Victory <chikamagozi25@gmail.com>
2022-12-17 20:20:29 +0100

    commenced with step2

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,5 @@
-console.log('hello world');
+const btn1 = document.querySelector('#submitBtn')
+
+btn1.addEventListener('click', () => {
+    console.log('Submit Clicked');
+})
--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,2 @@
-"use strict";console.log("hello world");
+"use strict";var btn1=document.querySelector("#submitBtn");btn1.addEventListener("click",(function(){console.log("Submit Clicked")}));
 //# sourceMappingURL=script.js.map
