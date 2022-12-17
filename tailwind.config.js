9529ee17214c646c1cb0ea821087a5a251045c2c
Nnadi Victory <chikamagozi25@gmail.com>
2022-12-17 23:44:02 +0100

    commenced with step3

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     content: ['./*.html', './app/js/*.js'],
     theme: {
       extend: {},
-    },
+  },
+  colors: {
+    'gray': 'hsl(231, 11%, 63%)'
+  },
     plugins: [],
   }
