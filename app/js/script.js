529ab24ff47853331314af33f25d16827604f88c
Nnadi Victory <chikamagozi25@gmail.com>
2022-12-20 15:05:28 +0100

    progress so far

--- app/js/script.js
+++ app/js/script.js
@@ -4,13 +4,13 @@ const nameInput = document.querySelector("#name");
 const email = document.querySelector("#email");
 const phone = document.querySelector("#phone");
 const personalInputs = [nameInput, email, phone];
-let formdata = {}
+const screenHeight = window.innerHeight;
+const btnCtn = document.querySelector(".btn__ctn");
+let formData = {}
 
 let step1 = () => {
   const btn1 = document.querySelector("#submitBtn");
-  const btnCtn = document.querySelector(".btn__ctn");
   const form = document.querySelector("#form");
-  const screenHeight = window.innerHeight;
   const testInput = document.querySelector(".test");
   const errorMessageName = document.querySelector("#errMsgName");
   const errorMessageEmail = document.querySelector("#errMsgEmail");
@@ -106,12 +106,12 @@ let step1 = () => {
     validateForm();
 
     if (step1Validated) {
-      formdata = {
+      formData = {
         name: nameInput.value,
         email: email.value,
         phone: phone.value,
       };
-      console.log(formdata);
+      console.log(formData);
       step2();
     } else {
         console.log('Not complete');
@@ -124,9 +124,9 @@ let step1 = () => {
 
 
 
-// STEP 2
+// * STEP 2 * //
 
-let yearlySelected = true
+let yearlySelected = false
 let step2Validated = false
 let prices = [9, 12, 15]
 let multiplied = false
@@ -147,9 +147,8 @@ let handleCheck2 = () => {
     const yearly = document.querySelector(".yearly");
     const monthly = document.querySelector(".monthly");
     const toggle = document.querySelector('.subSwitch')
-    toggle.checked = true
     if (yearlySelected == true) {
-        prices = prices.map(price => price * 10)
+        toggle.checked = true
         multiplied = true
         updateUIplans()
         monthly.classList.add('g')
@@ -189,6 +188,8 @@ let plansCheckedArr = [arcadeChecked, advancedChecked, proChecked]
 
 let handleSelectedPlan = (element, chkbox, eleId) => {
     element.classList.remove('hover')
+    element.classList.remove('active')
+
     
     
     
@@ -214,6 +215,7 @@ let handleSelectedPlan = (element, chkbox, eleId) => {
     } else if (chkbox.checked) {
         element.classList.remove('active')
         chkbox.checked = !chkbox.checked
+        console.log(chkbox.checked)
 
         if (eleId == 'arcade') {
             arcadeChecked = false
@@ -264,6 +266,8 @@ let validatePlansInputs = () => {
 
 }
 
+
+// !  Going to nextPage  , i.e: step3 //
 let nextFromPlans = () => {
     validatePlansInputs()
 
@@ -271,7 +275,154 @@ let nextFromPlans = () => {
 }
 
 
+
+// ! When 'go back' is clicked ; going back to step1 //
+
+let nameInputValue = ''
+let emailInputValue = ''
+let phoneInputValue = ''
+
+let renderButton = () => {
+    const btnCtn = document.querySelector(".btn__ctn");
+
+    btnCtn.innerHTML = `
+    <button onclick="validateForm2('${nameInputValue}', '${emailInputValue}', '${phoneInputValue}'); console.log(formData);" id="submitBtn" class="rounded-md py-3 px-6 text-white font-medium absolute z-40 bottom-3 lg:bottom-10 right-5 lg:right-16" type="submit">Next Step</button>
+    `
+}
+
+let handleInput = (element) => {
+    const screenHeight = window.innerHeight;
+    const btnCtn = document.querySelector(".btn__ctn");
+
+          if (screen.width <= 750) {
+            btnCtn.style.display = "none";
+            form.style.position = "absolute";
+          }
+
+          element.addEventListener("keyup", () => {
+            if (window.innerHeight === screenHeight) {
+              btnCtn.style.display = "block";
+              }
+              element.setAttribute('value', '')
+              element.removeAttribute('value', '')
+
+              if (element.name == 'name') {
+                  nameInputValue = element.value
+                  renderButton()
+              }
+              if (element.name == 'email') {
+                  emailInputValue = element.value
+                  renderButton()
+              }
+              if (element.name == 'phone') {
+                  phoneInputValue = element.value
+                  renderButton()
+              }
+          });
+
+          element.addEventListener("blur", () => {
+            btnCtn.style.display = "block";
+          });
+
+    window.addEventListener("resize", (e) => {
+            if (window.innerHeight === screenHeight) {
+              btnCtn.style.display = "block";
+            } else {
+              btnCtn.style.display = "none";
+            }
+          });
+}
+
+
+
+let nameValid2 = false;
+let emailValid2 = false;
+let phoneValid2 = false;
+let validations2 = [nameValid2, emailValid2, phoneValid2];
+let step1Validatedagain = false
+let updateValidState2 = () => {
+    validations2 = [nameValid2, emailValid2, phoneValid2];
+    if (validations2.includes(false)) {
+        step1Validatedagain = false
+    } else {
+        step1Validatedagain = true
+    }
+};
+
+
+
+
+
+let validateForm2 = (name, email, phone) => {
+    const errorMessageName = document.querySelector("#errMsgName");
+    const errorMessageEmail = document.querySelector("#errMsgEmail");
+    const errorMessagePhone = document.querySelector("#errMsgPhone");
+
+    if (name == "") {
+      nameValid2 = false;
+      updateValidState2();
+      errorMessageName.textContent = "This field is required";
+      setTimeout(() => {
+        errorMessageName.textContent = "";
+      }, 2000);
+    } else {
+      nameValid2 = true;
+      updateValidState2();
+    }
+
+    if (email == "") {
+      emailValid2 = false;
+      updateValidState2();
+      errorMessageEmail.textContent = "This field is required";
+      setTimeout(() => {
+        errorMessageEmail.textContent = "";
+      }, 2000);
+    } else if (!email.includes("@") || !email.includes(".")) {
+      emailValid2 = false;
+      updateValidState2();
+      errorMessageEmail.textContent = "Enter a valid email address";
+      setTimeout(() => {
+        errorMessageEmail.textContent = "";
+      }, 2000);
+    } else {
+      emailValid2 = true;
+      updateValidState2();
+    }
+
+    if (phone == "") {
+      phoneValid2 = false;
+      updateValidState2();
+      errorMessagePhone.textContent = "This field is required";
+      setTimeout(() => {
+        errorMessagePhone.textContent = "";
+      }, 2000);
+    } else {
+      phoneValid2 = true;
+      updateValidState2();
+    }
+    
+    if (step1Validatedagain) {
+        formData = {
+          name: name,
+          email: email,
+          phone: phone,
+        };
+        console.log(formData);
+        step2()
+        handleCheck2()
+      } else {
+          console.log('Not complete');
+      }
+  };
+
+
+// ! Go back to step1 function  //
 let goBacktoStep1 = () => {
+    nameInputValue = formData.name
+    emailInputValue = formData.email
+    phoneInputValue = formData.phone
+    console.log(formData)
+
     body.innerHTML = `
     <!-- side navbar for desktop -->
     <aside class="desktop__navbar hide-for-mobile pt-8 ml-5 fade-in">
@@ -342,33 +493,34 @@ let goBacktoStep1 = () => {
           <p>Name</p>
           <p id="errMsgName" class="errorMsg"></p>
         </div>
-        <input  class="w-full py-3 px-4 mb-4 font-medium" type="text" value=${formdata.name} placeholder="e.g. Stephen King" name="name" id="name">
+        <input onfocus="handleInput(this)" class="w-full py-3 px-4 mb-4 font-medium" type="text" value='${nameInputValue}' placeholder="e.g. Stephen King" name="name" id="name">
 
         
         <div class="label flex justify-between lg:font-medium mt-5">
           <p>Email Address</p>
           <p id="errMsgEmail" class="errorMsg"></p>
         </div>
-        <input class="w-full py-3 px-4 mb-4 font-medium" value=${formdata.email} type="email" placeholder="e.g. stephenking@lorem.com" name="email" id="email">
+        <input onfocus="handleInput(this)" class="w-full py-3 px-4 mb-4 font-medium" value=${emailInputValue} type="email" placeholder="e.g. stephenking@lorem.com" name="email" id="email">
 
         
         <div class="label flex justify-between lg:font-medium mt-5">
           <p>Phone Number</p>
           <p id="errMsgPhone" class="errorMsg"></p>
         </div>
-        <!-- <input class="test" type="text"> -->
-        <input class="w-full py-3 px-4 mb-4 font-medium" type="number" value=${formdata.phone} placeholder="e.g. +1 234 567 890" name="phone number" id="phone">
+        <input  onfocus="handleInput(this)" class="w-full py-3 px-4 mb-4 font-medium" type="number" value=${phoneInputValue} placeholder="e.g. +1 234 567 890" name="phone" id="phone">
 
 
       </form>
 
       <div class="btn__ctn bg-white w-full z-40 h-20 absolute bottom-0 lg:w-1/2">
-        <button id="submitBtn" class="rounded-md py-3 px-6 text-white font-medium absolute z-40 bottom-3 lg:bottom-10 right-5 lg:right-16" type="submit">Next Step</button>
+        <button onclick="validateForm2('${nameInputValue}', '${emailInputValue}', '${phoneInputValue}'); console.log(formData);" id="submitBtn" class="rounded-md py-3 px-6 text-white font-medium absolute z-40 bottom-3 lg:bottom-10 right-5 lg:right-16" type="submit">Next Step</button>
       </div>
 
     </main>
     `
+    Object.keys(formData).forEach(key => delete formData[key])
 }
+
 
 
 let step2 = () => {
@@ -431,7 +583,7 @@ let step2 = () => {
 
     <main class="lg:w-1/2 self-start my-11 pt-4 ">
 
-      <div id="plans" class="fade-in plans form fixed z-40 top-[6.45rem] inset-x-0 mx-auto rounded-md lg:relative px-7 py-6 lg:top-0 lg:py-2">
+      <div id="plans" class="fade-in plans form fixed z-40 top-[6.65rem] inset-x-0 mx-auto rounded-md lg:relative px-7 py-6 lg:top-0 lg:py-2">
         <div class="plans__header">
             <h1 class="text-3xl font-bold py-1 lg:text-4xl"> Select your plan</h1>
             <p class="description text-lg py-0">
@@ -452,7 +604,7 @@ let step2 = () => {
                     <p id="arcadePrice" class="text-gray-400 font-medium">$9/mo</p>
                     <p class="font-medium"></p>
                 </div>
-                <input class="invisible absolute" type="checkbox" name="arcade" id="arcadeChkBox">
+                <input class="invisible absolute" type="checkbox" ${arcadeChecked ? 'checked' : ''} name="arcade" id="arcadeChkBox">
             </div>
 
             <div onclick="handleSelectedPlan(this, advancedChkBox, this.id)" id="advanced" class="plans__ctn--card hover lg:w-1/3 sm:w-full flex lg:items-start items-center lg:flex-col py-4 my-3 lg:gap-y-14 gap-x-5 lg:py-5 relative px-6 ${advancedChecked ? 'active' : ''}">
@@ -464,7 +616,7 @@ let step2 = () => {
                     <p id="advancedPrice" class="text-gray-400 font-medium"> $12/mo</p>
                     <p class="font-medium"></p>
                 </div>
-                <input class="invisible absolute" type="checkbox" name="advanced" id="advancedChkBox">
+                <input class="invisible absolute" type="checkbox" ${advancedChecked ? 'checked' : ''} name="advanced" id="advancedChkBox">
             </div>
 
             <div onclick="handleSelectedPlan(this, proChkBox, this.id)" id="pro" class="plans__ctn--card hover lg:w-1/3 sm:w-full flex lg:items-start items-center lg:flex-col py-4 my-3 lg:gap-y-14 gap-x-5 lg:py-5 relative px-6 ${proChecked ? 'active' : ''}">
@@ -476,7 +628,7 @@ let step2 = () => {
                     <p id="proPrice" class="text-gray-400 font-medium">$15/mo</p>
                     <p class="font-medium"></p>
                 </div>
-                <input class="invisible absolute" type="checkbox" name="pro" id="proChkBox">
+                <input class="invisible absolute" type="checkbox" ${proChecked ? 'checked' : ''} name="pro" id="proChkBox">
             </div>
 
         </div>
@@ -501,6 +653,67 @@ let step2 = () => {
 
     `
 }
+
+
+
+// ! STEP 3
+
+let addOnsMonthly = [1, 2, 2]
+let addOnsYearly = addOnsMonthly.map(price => price * 10)
+let onlineServiceChecked = false
+let largeStorageChecked = false
+let customProfileChecked = false
+
+let addOnsQuery = [onlineServiceChecked, largeStorageChecked, customProfileChecked]
+
+let updateAddOns = () => {
+    addOnsQuery = [onlineServiceChecked, largeStorageChecked, customProfileChecked]
+}
+
+let handleAddOnContainer = (container) => {
+    const elementChkBox = container.children[2]
+    container.classList.remove('hover')
+    if (elementChkBox.checked) {
+        container.classList.add('active')
+    } else {
+        container.classList.remove('active')
+        container.addEventListener('mouseleave', () => container.classList.add('hover'))
+    }
+}
+
+let handleAddOns = (checkBox) => {
+
+    if (checkBox.checked) {
+        if (checkBox.id.toLowerCase().includes('onlineservice')) {
+            onlineServiceChecked = true
+            updateAddOns()
+        }
+        if (checkBox.id.toLowerCase().includes('largerstorage')) {
+            largeStorageChecked = true
+            updateAddOns()
+        }
+        if (checkBox.id.toLowerCase().includes('profile')) {
+            customProfileChecked = true
+            updateAddOns()
+        }
+
+    } else if (!checkBox.checked) {
+        if (checkBox.id.toLowerCase().includes('onlineservice')) {
+            onlineServiceChecked = false
+            updateAddOns()
+        }
+        if (checkBox.id.toLowerCase().includes('largerstorage')) {
+            largeStorageChecked = false
+            updateAddOns()
+        }
+        if (checkBox.id.toLowerCase().includes('profile')) {
+            customProfileChecked = false
+            updateAddOns()
+        }
+    }
+}
+
+
 
 
 let step3 = () => {
@@ -563,7 +776,7 @@ let step3 = () => {
 
     <main class="lg:w-1/2 self-start my-11 pt-4">
 
-      <div id="add-ons" class=" addOns form fixed z-40 top-[6.65rem] inset-x-0 mx-auto rounded-md lg:relative px-7 py-6 lg:top-0 lg:py-2">
+      <div id="add-ons" class="fade-in addOns form fixed z-40 top-[6.65rem] inset-x-0 mx-auto rounded-md lg:relative px-7 py-6 lg:top-0 lg:py-2">
         <div class="addOns__header">
             <h1 class="text-3xl font-bold py-1 lg:text-4xl"> Pick add-ons</h1>
             <p class="description text-lg py-0">
@@ -573,39 +786,39 @@ let step3 = () => {
 
         <div class="addOns__ctn lg:mr-14 lg:mt-9 mt-4">
 
-            <label class="container active addOns__ctn--card flex justify-between px-3 lg:px-11 my-4 py-4 items-center pl-12 lg:pl-20">
+            <label onclick="handleAddOnContainer(this)"  class="container ${onlineServiceChecked ? 'active' : ''} hover addOns__ctn--card flex justify-between px-3 lg:px-11 my-4 py-4 items-center pl-12 lg:pl-20">
                 <div class="addOns__ctn--card__header">
                     <h3 class="font-semibold">Online service</h3>
                     <p class="text-gray-400">Access to multiplayer games</p>
                 </div>
                 <div class="addOns__ctn--card__priceCtn">
-                    <p id="price" class="price font-medium">+$1/mo</p>
+                    <p id="price" class="price font-medium">+$${yearlySelected ? addOnsYearly[0] : addOnsMonthly[0]}/mo</p>
                 </div>
-                <input type="checkbox" checked id="chkForOnlineService">
+                <input type="checkbox" ${onlineServiceChecked ? 'checked' : ''} onchange="handleAddOns(this)" id="chkForOnlineService">
                 <span class="checkmark left-1 lg:left-5"></span>
               </label>
 
-            <label class="container active addOns__ctn--card flex justify-between px-3 lg:px-11 my-4 py-4 items-center pl-12 lg:pl-20">
+            <label onclick="handleAddOnContainer(this)" class="container ${largeStorageChecked ? 'active' : ''} hover addOns__ctn--card flex justify-between px-3 lg:px-11 my-4 py-4 items-center pl-12 lg:pl-20">
                 <div class="addOns__ctn--card__header">
                     <h3 class="font-semibold">Larger storage</h3>
                     <p class="text-gray-400">Extra 1TB of cloud save</p>
                 </div>
                 <div class="addOns__ctn--card__priceCtn">
-                    <p id="price" class="price font-medium">+$2/mo</p>
+                    <p id="price" class="price font-medium">+$${yearlySelected ? addOnsYearly[1] : addOnsMonthly[1]}/mo</p>
                 </div>
-                <input type="checkbox" checked id="chkForLargerStorage">
+                <input type="checkbox" ${largeStorageChecked ? 'checked' : ''} onchange="handleAddOns(this)" id="chkForLargerStorage">
                 <span class="checkmark left-1 lg:left-5"></span>
               </label>
 
-            <label class="container addOns__ctn--card flex justify-between px-3 lg:px-11 my-4 py-4 items-center pl-12 lg:pl-20">
+            <label onclick="handleAddOnContainer(this)" ${customProfileChecked ? 'active' : ''} class="container hover addOns__ctn--card flex justify-between px-3 lg:px-11 my-4 py-4 items-center pl-12 lg:pl-20">
                 <div class="addOns__ctn--card__header">
                     <h3 class="font-semibold">Customizable Profile</h3>
                     <p class="text-gray-400 ">Custom theme on your profile</p>
                 </div>
                 <div class="addOns__ctn--card__priceCtn">
-                    <p id="price" class="price font-medium">+$2/mo</p>
+                    <p id="price" class="price font-medium">+$${yearlySelected ? addOnsYearly[2] : addOnsMonthly[2]}/mo</p>
                 </div>
-                <input type="checkbox" id="chkForCustProfile">
+                <input type="checkbox" ${customProfileChecked ? 'checked' : ''} onchange="handleAddOns(this)" id="chkForCustProfile">
                 <span class="checkmark left-1 lg:left-5"></span>
               </label>
 
@@ -614,7 +827,7 @@ let step3 = () => {
     </div>
 
       <div class="btn__ctn z-50 bg-white w-full h-[70px] absolute bottom-0 lg:w-1/2">
-        <button id="goBackBtn1" class=" goBackBtn py-3 font-medium absolute z-40 bottom-3 lg:bottom-10 left-5 lg:left-16" type="submit">Go Back</button>
+        <button onclick="step2()" id="goBackBtn2" class=" goBackBtn py-3 font-medium absolute z-40 bottom-3 lg:bottom-10 left-5 lg:left-16" type="submit">Go Back</button>
         <button id="submitBtn" class="rounded-md py-3 px-6 text-white font-medium absolute z-40 bottom-3 lg:bottom-10 right-5 lg:right-16" type="submit">Next Step</button>
       </div>
 
